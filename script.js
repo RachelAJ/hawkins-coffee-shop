481567c9a4c3f8de73c7e5858b79166d26d515e7
Rachel Johnson <rjohns0783@gmail.com>
2020-07-14 18:41:43 -0400

    Formatte HTML Form and added JS function

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-// alert('Welcome to Coffee & Contemplation! Make sure to sign up for our emails!')
 
-let btn = document.querySelector(".submit");
+const myForm = document.getElementById('myForm');
+const myName = document.getElementById('myName');
+const myEmail = document.getElementById('myEmail');
+const myButton = document.getElementById('myButton');
+
+// function validateEmail(myEmail) { 
+//     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(myEmail);
 
 function buttonClick(e) {
     e.preventDefault();
-    btn.innerHTML = "Thank you! We'll be in touch!";
+    myButton.innerHTML = "See you in the upsidedown..";
+    myButton.style.color = 'red';
+    myName.value = '';
+    myEmail.value = '';
 }
 
-btn.addEventListener("click", buttonClick);
+myButton.addEventListener("click", buttonClick);
+
+
+
+
+
+
+
+
+
+
+
+
+// function buttonClick(e) {
+//     e.preventDefault();
+//     myButton.innerHTML = "See you in the upsidedown..";
+//     myName.value = '';
+//     myEmail.value = '';
+// }
+
+// myButton.addEventListener('click', () => {
+//     myButton.style.color = 'red';
+// });
+
+// myButton.addEventListener("click", buttonClick);
